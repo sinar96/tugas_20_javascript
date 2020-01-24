1169f8e6f1f863ba4a67ff675478007123b781f9
sinar96 <sinar96>
2020-01-24 20:17:10 +0800

    match js

---
+++ tugas20.js
@@ -0,0 +1,6 @@
+function Cari() {
+    var str = "There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable."
+    console.log(str.match("are"));
+    console.log(str.match(/words/));
+}
+Cari();
